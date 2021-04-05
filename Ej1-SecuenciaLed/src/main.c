03a201001a84ede386e98c74b06df55ce29a6ed9
francolombatti <colombattifhhmm@gmail.com>
2021-04-05 15:29:40 -0300

    cambios en secuencia led

--- Ej1-SecuenciaLed/src/main.c
+++ Ej1-SecuenciaLed/src/main.c
@@ -1,1 +1,18 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+
+void app_main() {
+gpio_pad_select_gpio(BLINK_GPIO);
+gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+	printf("Turning off the LED\n");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+	printf("Turning on the LED\n");
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
